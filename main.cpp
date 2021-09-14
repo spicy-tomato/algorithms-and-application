ad98e09d2d9763253faea6217fea425f7610c076
annguyen <annguyen.it.utc@gmail.com>
2021-09-14 22:06:52 +0700

    Red Black BST: Delete

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include "red_black_tree/red_black_tree.cpp"
+#include <map>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    RedBlackTree<int, int> tree;
+
+    for (int i = 0; i < 5; ++i) {
+        tree.insert(i, i*i);
+    }
+
+    std::cout << tree.get(2) << '\n';
+    std::cout << tree.size() << '\n';
+    tree.erase(3);
+    std::cout << tree.size() << '\n';
+
+    std::map<int, int> m;
+
     return 0;
 }
--- red_black_tree/red_black_tree.cpp
+++ red_black_tree/red_black_tree.cpp
@@ -1,4 +1,92 @@
-#include "red_black_tree.h"
+#include <iostream>
+
+#ifndef RED_BLACK_TREE
+#define RED_BLACK_TREE
+
+template<class Key, class Value>
+class RedBlackTree {
+    private:
+        // Static constants
+        static const bool RED   = true;
+        static const bool BLACK = false;
+
+        // Inner class
+        class Node {
+            public:
+                // Node's props
+                Key   key;
+                Value value;
+                Node* left  = nullptr;
+                Node* right = nullptr;
+                int   N     = 0;
+                bool  color = RED;
+
+                // Node's constructor
+                Node(Key key, Value value, int N, bool color);
+
+                void assign(Node* node);
+        };
+
+        // Props
+        Node* root = nullptr;
+
+        // Private methods
+        Node* rotateLeft(Node* head);
+
+        Node* rotateRight(Node* head);
+
+        void flipColors(Node* head);
+
+        int size(Node* head);
+
+        int updateSize(Node* head);
+
+        bool isRed(Node* head);
+
+        bool isBlack(Node* head);
+
+        Node* insert(Node*& head, Key key, Value value);
+
+        Node* erase(Node* head, Key key);
+
+        void clear(Node* head);
+
+        Node* balance(Node* head);
+
+        Node* moveRedLeft(Node* head);
+
+        Node* moveRedRight(Node* head);
+
+        Node* eraseMin(Node* head);
+
+        Node* eraseMax(Node* head);
+
+        Node* min(Node* head);
+
+    public:
+        // Constructor
+        RedBlackTree();
+
+        // Destructor
+        ~RedBlackTree();
+
+        // Methods
+        int size();
+
+        int isEmpty();
+
+        Value get(Key key);
+
+        void insert(Key key, Value value);
+
+        void erase(Key key);
+
+        void eraseMin();
+
+        void eraseMax();
+
+        void clear();
+};
 
 template<class Key, class Value>
 RedBlackTree<Key, Value>::Node::Node(Key key, Value value, int N, bool color) {
@@ -9,72 +97,226 @@ RedBlackTree<Key, Value>::Node::Node(Key key, Value value, int N, bool color) {
 }
 
 template<class Key, class Value>
+void RedBlackTree<Key, Value>::Node::assign(Node* node) {
+    key   = node->key;
+    value = node->value;
+}
+
+template<class Key, class Value>
 RedBlackTree<Key, Value>::RedBlackTree() = default;
 
 template<class Key, class Value>
-typename RedBlackTree<Key, Value>::Node RedBlackTree<Key, Value>::rotateLeft(RedBlackTree<Key, Value>::Node h) {
-    Node x = h->right;
+RedBlackTree<Key, Value>::~RedBlackTree() {
+    clear();
+}
 
-    h->right = x->left;
-    x->left  = h;
+template<class Key, class Value>
+typename RedBlackTree<Key, Value>::Node* RedBlackTree<Key, Value>::rotateLeft(RedBlackTree<Key, Value>::Node* head) {
+    if (head->right == nullptr) {
+        return head;
+    }
 
-    x->color = h->color;
-    h->color = RED;
+    Node* x = head->right;
 
-    x->N = h->N;
-    h->N = size(x->left) + size(x->right) + 1;
+    head->right = x->left;
+    x->left     = head;
+
+    x->color    = head->color;
+    head->color = RED;
+
+    x->N    = head->N;
+    head->N = size(head->left) + size(head->right) + 1;
 
     return x;
 }
 
 template<class Key, class Value>
-typename RedBlackTree<Key, Value>::Node RedBlackTree<Key, Value>::rotateRight(RedBlackTree<Key, Value>::Node h) {
-    Node x = h->left;
+typename RedBlackTree<Key, Value>::Node* RedBlackTree<Key, Value>::rotateRight(RedBlackTree<Key, Value>::Node* head) {
+    if (head->left == nullptr) {
+        return head;
+    }
+    Node* x = head->left;
 
-    h->left  = x->right;
-    x->right = h;
+    head->left = x->right;
+    x->right   = head;
 
-    x->color = h->color;
-    h->color = RED;
+    x->color    = head->color;
+    head->color = RED;
 
-    x->N = h->N;
-    h->N = size(x->right) + size(x->left) + 1;
+    x->N    = head->N;
+    updateSize(head);
 
     return x;
 }
 
 template<class Key, class Value>
-void RedBlackTree<Key, Value>::flipColors(RedBlackTree<Key, Value>::Node h) {
-    h->color = RED;
-    h->left->color = h->right->color = BLACK;
+void RedBlackTree<Key, Value>::flipColors(RedBlackTree<Key, Value>::Node* head) {
+    head->color       = RED;
+    head->left->color = head->right->color = BLACK;
 }
 
 template<class Key, class Value>
-int RedBlackTree<Key, Value>::size(RedBlackTree<Key, Value>::Node h) {
-    return h->N;
+int RedBlackTree<Key, Value>::size(RedBlackTree<Key, Value>::Node* head) {
+    return head != nullptr ? head->N : 0;
 }
 
 template<class Key, class Value>
-bool RedBlackTree<Key, Value>::isRed(RedBlackTree<Key, Value>::Node h) {
-    return h->color && h->color == RED;
+int RedBlackTree<Key, Value>::updateSize(RedBlackTree::Node* head) {
+    head->N = size(head->right) + size(head->left) + 1;
+    return head->N;
 }
 
 template<class Key, class Value>
-typename RedBlackTree<Key, Value>::Node RedBlackTree<Key, Value>::put(RedBlackTree<Key, Value>::Node h, Key key, Value value) {
-    if (h == nullptr){
+bool RedBlackTree<Key, Value>::isRed(RedBlackTree<Key, Value>::Node* head) {
+    return head != nullptr && head->color == RED;
+}
+
+template<class Key, class Value>
+bool RedBlackTree<Key, Value>::isBlack(RedBlackTree<Key, Value>::Node* head) {
+    return !isRed(head);
+}
+
+template<class Key, class Value>
+typename RedBlackTree<Key, Value>::Node* RedBlackTree<Key, Value>::insert(RedBlackTree<Key, Value>::Node*& head, Key key, Value value) {
+    if (head == nullptr) {
         return new Node(key, value, 1, RED);
     }
 
-    if      (key < h.key) h.left  = put(h.left , key, value);
-    else if (key > h.key) h.right = put(h.right, key, value);
-    else h.value = value;
+    if      (key < head->key) head->left  = insert(head->left , key, value);
+    else if (key > head->key) head->right = insert(head->right, key, value);
+    else     head->value = value;
 
-    if (isRed(h.right) && !isRed(h.left))      h = rotateLeft(h);
-    if (isRed(h.left)  &&  isRed(h.left.left)) h = rotateRight(h);
-    if (isRed(h.left)  &&  isRed(h.right))     flipColors(h);
+    if (isRed(head->right) && isBlack(head->left))     head = rotateLeft(head);
+    if (isRed(head->left)  && isRed(head->left->left)) head = rotateRight(head);
+    if (isRed(head->left)  && isRed(head->right))      flipColors(head);
 
-    h.N = size(h.left) + size(h.right) + 1;
-    return h;
+    head->N = size(head->left) + size(head->right) + 1;
+    return head;
+}
+
+template<class Key, class Value>
+typename RedBlackTree<Key, Value>::Node* RedBlackTree<Key, Value>::erase(RedBlackTree<Key, Value>::Node* head, Key key) {
+    if (key < head->key) {
+        if (isBlack(head->left) && isBlack(head->left->left)) {
+            head = moveRedLeft(head);
+        }
+
+        head->left = erase(head->left, key);
+    }
+    else {
+        if (isRed(head->left)) {
+            head = rotateRight(head);
+        }
+
+        if (head->key == key && head->right == nullptr) {
+            return nullptr;
+        }
+
+        if (isBlack(head->right) && isBlack(head->right->left)) {
+            head = moveRedRight(head);
+        }
+
+        if (head->key == key) {
+            Node* x = min(head->right);
+            head->assign(x);
+            head->right = eraseMin(head->right);
+        }
+        else {
+            head->right = erase(head->right, key);
+        }
+    }
+
+    updateSize(head);
+
+    return balance(head);
+}
+
+template<class Key, class Value>
+void RedBlackTree<Key, Value>::clear(RedBlackTree<Key, Value>::Node* head) {
+    if (head->left != nullptr) {
+        clear(head->left);
+    }
+
+    if (head->right != nullptr) {
+        clear(head->right);
+    }
+
+    delete head;
+}
+
+template<class Key, class Value>
+typename RedBlackTree<Key, Value>::Node* RedBlackTree<Key, Value>::balance(Node* head) {
+    if (isRed(head)) {
+        head = rotateLeft(head);
+    }
+
+    return head;
+}
+
+template<class Key, class Value>
+typename RedBlackTree<Key, Value>::Node* RedBlackTree<Key, Value>::moveRedLeft(RedBlackTree<Key, Value>::Node* head) {
+    flipColors(head);
+
+    if (isRed(head->right->left)) {
+        head->right = rotateRight(head->right);
+        head = rotateLeft(head);
+    }
+
+    return head;
+}
+
+template<class Key, class Value>
+typename RedBlackTree<Key, Value>::Node* RedBlackTree<Key, Value>::moveRedRight(RedBlackTree<Key, Value>::Node* head) {
+    flipColors(head);
+
+    if (isRed(head->left->left)) {
+        head = rotateRight(head->right);
+    }
+
+    return head;
+}
+
+template<class Key, class Value>
+typename RedBlackTree<Key, Value>::Node* RedBlackTree<Key, Value>::eraseMin(RedBlackTree<Key, Value>::Node* head) {
+    if (head->left == nullptr) {
+        return nullptr;
+    }
+
+    if (isBlack(head->left) && isBlack(head->left->left)) {
+        head = moveRedLeft(head);
+    }
+
+    head->left = eraseMin(head->left);
+
+    return balance(head);
+}
+
+template<class Key, class Value>
+typename RedBlackTree<Key, Value>::Node* RedBlackTree<Key, Value>::eraseMax(RedBlackTree<Key, Value>::Node* head) {
+    if (isRed(head->left)) {
+        head = rotateRight(head);
+    }
+
+    if (head->right == nullptr) {
+        return nullptr;
+    }
+
+    if (isBlack(head->right) && isBlack(head->right->left)) {
+        head = moveRedRight(head);
+    }
+
+    head->right = eraseMax(head->right);
+
+    return balance(head);
+}
+
+template<class Key, class Value>
+typename RedBlackTree<Key, Value>::Node* RedBlackTree<Key, Value>::min(RedBlackTree<Key, Value>::Node* head) {
+    Node* minNode = head;
+    while (minNode->left != nullptr) {
+        minNode = minNode->left;
+    }
+    return minNode;
 }
 
 template<class Key, class Value>
@@ -83,8 +325,77 @@ int RedBlackTree<Key, Value>::size() {
 }
 
 template<class Key, class Value>
-void RedBlackTree<Key, Value>::put(Key key, Value value) {
-    root = put(root, key, value);
+int RedBlackTree<Key, Value>::isEmpty() {
+    return size() == 0;
+}
+
+template<class Key, class Value>
+Value RedBlackTree<Key, Value>::get(Key key) {
+    Node* curr = root;
+    while (curr != nullptr) {
+        if (key == curr->key) {
+            return curr->value;
+        }
+
+        if (key < curr->key) {
+            curr = curr->left;
+        }
+        else {
+            curr = curr->right;
+        }
+    }
+
+    throw std::out_of_range("Cannot find");
+}
+
+template<class Key, class Value>
+void RedBlackTree<Key, Value>::insert(Key key, Value value) {
+    root = insert(root, key, value);
     root->color = BLACK;
 }
 
+template<class Key, class Value>
+void RedBlackTree<Key, Value>::erase(Key key) {
+    if (isBlack(root->left) && isBlack(root->right)) {
+        root->color = RED;
+    }
+
+    root = erase(root, key);
+
+    if (!isEmpty()) {
+        root->color = BLACK;
+    }
+}
+
+template<class Key, class Value>
+void RedBlackTree<Key, Value>::eraseMin() {
+    if (isBlack(root->left) && isBlack(root->right)) {
+        root->color = RED;
+    }
+
+    root = eraseMin(root);
+
+    if (!isEmpty()) {
+        root->color = BLACK;
+    }
+}
+
+template<class Key, class Value>
+void RedBlackTree<Key, Value>::eraseMax() {
+    if (isBlack(root->left) && isBlack(root->right)) {
+        root->color = RED;
+    }
+
+    root = eraseMax(root);
+
+    if (!isEmpty()) {
+        root->color = BLACK;
+    }
+}
+
+template<class Key, class Value>
+void RedBlackTree<Key, Value>::clear() {
+    clear(root);
+}
+
+#endif //RED_BLACK_TREE
