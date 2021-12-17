d33db2b50c3bcb54cfa1b5ea637b26928767e972
annguyen <annguyen.it.utc@gmail.com>
2021-12-17 16:13:23 +0700

    Update Ex4

--- exercise/4.py
+++ exercise/4.py
@@ -1,4 +1,4 @@
-def hash_table_quadratic(arr: [int], n) -> [int]:
+def hash_table_chaining(arr: [int], n) -> [int]:
     res = [[] for _ in range(n)]
     for num in arr:
         res[num % n].append(num)
@@ -7,7 +7,7 @@ def hash_table_quadratic(arr: [int], n) -> [int]:
 
 def run():
     arr = [4371, 1323, 6173, 4199, 4344, 9679, 1989]
-    print(hash_table_quadratic(arr, 10))
+    print(hash_table_chaining(arr, 10))
 
 
 run()
