e33a97abffb01e6df40ef8c536553974cdb18cd1
annguyen <annguyen.it.utc@gmail.com>
2021-12-18 09:13:00 +0700

    Update Ex13

--- exercise/13.py
+++ exercise/13.py
@@ -1,33 +1,33 @@
 def solve(arr: list[tuple[int, int]]) -> int:
     acc = [t[0] - t[1] for t in arr]
-    i = 0
-    while acc[i] < 0 and i < len(arr):
-        i += 1
-    if i == len(arr):
+    start = 0
+    while acc[start] < 0 and start < len(arr):
+        start += 1
+    if start == len(arr):
         return -1
 
-    s = acc[i]
-    j = i + 1
-    while i != j:
-        if i == len(arr):
+    s = acc[start]
+    end = start + 1
+    while start != end:
+        if start == len(arr):
             return -1
 
-        if j == len(arr):
-            j = 0
+        if end == len(arr):
+            end = 0
 
-        s += acc[j]
+        s += acc[end]
         if s < 0:
-            if j < i:
+            if end < start:
                 return -1
-            i = j + 1
-            while acc[i] < 0 and i < len(arr):
-                i += 1
-            if i == len(arr):
+            start = end + 1
+            while acc[start] < 0 and start < len(arr):
+                start += 1
+            if start == len(arr):
                 return -1
 
-            s = acc[i]
-        j += 1
-    return i
+            s = acc[start]
+        end += 1
+    return start
 
 
 data = [(4, 6), (6, 5), (7, 3), (0, 6), (10, 2), (0, 1)]
